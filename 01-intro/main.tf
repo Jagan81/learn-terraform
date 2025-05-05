a4b1f08ae16ea91f7cd875e6fd157dd80d9f5054
Jagannnnnn <jaganmohan85220@gmail.com>
2025-05-05 16:05:10 +0530

    roboshop code

--- 01-intro/main.tf
+++ 01-intro/main.tf
@@ -4,3 +4,22 @@ resource "local_file" "foo" {
   content = "Hello world"
   filename = "/tmp/foo.bar"
 }
+
+# Normal Variables
+
+variable "x" {
+  default = 10
+}
+
+
+## Print Variable on output
+output "x" {
+  value = var.x
+}
+
+
+
+# if our value is just variable then var.x, if it is combination of strings then "$(var.x)
+output "x1" {
+  value = "Value of x - ${var.x}"
+}
